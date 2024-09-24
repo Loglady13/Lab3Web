6f2565d733ce82b744e3b37a5a150cbde136ae20
Loglady13 <raschelljq0203@estudiantec.cr>
2024-09-24 15:35:59 -0600

    Update AddTaskForm.jsx

--- lab3/src/components/AddTaskForm.jsx
+++ lab3/src/components/AddTaskForm.jsx
@@ -3,7 +3,10 @@
 const AddTaskFrom = () =>{
     return(
         <div>
-
+            <form action="">
+                <label htmlFor="task-title">Task Title</label>
+                <input type="text" id='task-title'/>
+            </form>
         </div>
     );
 }
