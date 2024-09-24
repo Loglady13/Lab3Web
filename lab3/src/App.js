02f5a0786edbf3d85ba2e8921ce2dbc87d345887
Anthony Arias <anthonyariasr@outlook.com>
2024-09-24 15:35:00 -0600

    tailwind config

--- lab3/src/App.js
+++ lab3/src/App.js
@@ -1,22 +1,16 @@
 import './output.css';
+import NavigationBar from "./components/NavigationBar"
+import TaskDetail from './components/TaskDetails';
+import TaskList from './components/TaskList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Sharon Guapap
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavigationBar/>
+      <div className='flex w-full'>
+        <TaskList/>
+        <TaskDetail/>
+      </div>
     </div>
   );
 }
