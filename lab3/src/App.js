cc9951f5736f1e6b4d82d181d03c8ad838ddfd06
AnthonyAriass <anthonyarias2000@estudiantec.cr>
2024-09-24 20:46:17 -0600

    initial UI setup

--- lab3/src/App.js
+++ lab3/src/App.js
@@ -3,13 +3,14 @@ import NavigationBar from "./components/NavigationBar"
 import TaskDetail from './components/TaskDetails';
 import TaskList from './components/TaskList';
 
+
 function App() {
   return (
     <div className="App">
       <NavigationBar/>
       <div className='flex w-full'>
         <TaskList/>
-        <TaskDetail/>
+        <TaskDetail task_title={"Example"} task_date={"29/11/2024"} task_description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"}/>
       </div>
     </div>
   );
